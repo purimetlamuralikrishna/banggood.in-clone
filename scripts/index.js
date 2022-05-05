3f8abf4a974eceb5fb3af5fc0e40f31109f7eb9d
Avinash7249 <ghatanureavi@gmail.com>
2022-05-05 13:15:07 +0530

    footer added in home

--- scripts/index.js
+++ scripts/index.js
@@ -227,6 +227,97 @@ let Recommendations=[
   price:"21,00€",
   dis:"Stitching Design Holiday S...",
 },
+{
+  img:"https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/9D/9B/12fa5226-9825-4e8c-aac2-016dde53068e.jpg.webp",
+  price:"299,82€",
+  dis:"Artillery® GeniusPro & Geni...",
+},
+{
+  img:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/A6/21/d5ff9e20-b0e7-d586-98a4-7a1dcaf64075.jpg.webp",
+  price:"12,41€",
+  dis:"10X 15X 20X 25X Spectacle...",
+},
+{
+  img:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/ser1/banggood/images/58/CF/72f73973-140d-4d7b-9630-c305d9926327.jpg.webp",
+  price:"94,53€",
+  dis:"Wltoys 144001 1/14 2.4G 4W...",
+},
+{
+  img:"https://imgaz.staticbg.com/thumb/gallery/oaupload/banggood/images/C7/76/7b5468a9-5dfa-4e82-afd3-924854404fc4.jpg.webp",
+  price:"36,28€",
+  dis:"[bluetooth Call] Zeblaze G...",
+},
+{
+  img:"https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/18/0F/c78eab87-d57e-4fd2-a594-6682c068aba9.jpg.webp",
+  price:"400,09€",
+  dis:"Artillery® Sidewinder X2 &...",
+},
+{
+  img:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/D4/23/d59c1b22-3336-4f9b-88d1-1fc69dcf4a9f.jpg.webp",
+  price:"133,69€",
+  dis:"Blackview TAB 8 SC9863A...",
+},
+{
+  img:"https://imgaz.staticbg.com/thumb/gallery/oaupload/banggood/images/38/E4/03761ea6-8ffe-4eee-8b53-1d382ca485b3.jpg.webp",
+  price:"28,64€",
+  dis:"MUSTOOL MT009 4.4-Inch T...",
+},
+{
+  img:"https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/3F/6B/96f4f4d4-7f67-4589-a80a-52dabb132e06.jpg.webp",
+  price:"38,19€",
+  dis:"ELEGIANT C520 BT 5.0 1.3...",
+},
+{
+  img:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/35/6D/3b538ac9-5456-45a0-b98d-d87c8c87923b.jpg.webp",
+  price:"79,25€",
+  dis:"WPL C24 1/16 2.4G 4WD...",
+},
+{
+  img:"https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/79/84/81fb6f5b-0749-493d-85ba-a630e3c3d4db.png.webp",
+  price:"190,04€",
+  dis:"Artillery®Hornet 3D Printe...",
+},
+{
+  img:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/ser1/banggood/images/89/B9/5ceeb020-a11d-4d0d-a289-3301de65d7ab.jpg.webp",
+  price:"69,70€",
+  dis:"MUSTOOL G1200 Digital M...",
+},
+{
+  img:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/CA/A2/293e68f5-3a27-4ee3-a166-bf3d0a89c80b.jpg.webp",
+  price:"665,62€",
+  dis:"Anycubic® Photon Mono...",
+},
+{
+  img:"https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/AF/46/8ed8639b-6bb0-42d4-a455-cb5ef1ed2770.jpg.webp",
+  price:"28,64€",
+  dis:"Zeblaze NEO 1.3 inch Full-R...",
+},
+{
+  img:"https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/55/B9/56fe86a8-a6a5-4d55-9f33-ff5949a65356.jpg.webp",
+  price:"228,24€",
+  dis:"DOOGEE S96 Pro Global B...",
+},
+{
+  img:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/F3/F0/4dc41a52-21f2-4e98-95d0-f6b70195aa25.jpg.webp",
+  price:"15,27€",
+  dis:"DANIU Digital Stainless Ste...",
+},
+{
+  img:"https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/5A/4D/1ce6c959-0963-4aa5-b558-fad6d5d0f7be.jpg.webp",
+  price:"36,28€",
+  dis:"[30 Days Standby]Zeblaze G...",
+},
+{
+  img:"https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/67/E5/22493415-7e69-4c90-a3af-2620beb8070d.jpg.webp",
+  price:"339,96€",
+  dis:"XMUND XD-T1 Treadmill Wal...",
+},
+{
+  img:"https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/23/02/9b85d760-1725-4221-9d13-7b0f51de4bfc.jpg.webp",
+  price:"21,00€",
+  dis:"Stitching Design Holiday S...",
+},
+
 
 ];
 
