bc7baf85839c4ed2a452b332c4169bec03fee622
Abhay Kumar Pandey <abhaykrpndy@gmail.com>
2022-05-07 09:41:05 +0530

    electronic page

---
+++ phones/Phones_Accesories.js
@@ -0,0 +1,156 @@
+var mensData = [
+    {
+      image_url:
+        "https://imgaz.staticbg.com/thumb/gallery/oaupload/banggood/images/25/C7/dff41616-f508-4d9a-9279-9b43aee4ba5b.jpg.webp",
+      name: "LOKMAT APPLLP Max 4GB+64GB 2.88inch Detachable Big Screen ",
+      price: 11219,
+    },
+    {
+      image_url:
+        "https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/3E/8E/8d3c66b4-83f5-42dc-863b-5a65830275c5.jpg.webp",
+      name: "Rogbid Air 1.75 inch HD Screen Dual Mode Dual Chip 4G Watch",
+      price: 10019,
+    },
+    {
+      image_url:
+        "https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/DF/72/6c64aa2e-f788-43df-a322-9da6b9251d75.jpg.webp",
+      name: " Aux L/R Wireless Stereo Audio Adapter Stereo Audio Adapter",
+      price: 1575,
+    },
+    {
+      image_url:
+        " https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/B4/8D/41da7abb-aa68-43fc-b169-41191b8fb4db.jpg.webp",
+      name: "   [Face Unlock]TICWRIS MAX 2.86 Inch HD Screen Smart Watch 3G+32G 4G-",
+      price: 34999,
+    },
+    {
+      image_url:
+        " https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/85/A8/4f80d6ee-dfd3-46c1-96f9-f493a49ab004.jpg.webp",
+      name: "BlitzWolf® BW-WA3 100W bluetooth Speaker Portable Speakers ",
+      price:1749,
+    },
+    {
+      image_url:
+        "https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/F8/8D/3e306acf-a642-4012-ad76-cd21164cbc3c.jpg.webp ",
+      name: " Tuya WiFi Smart LCD Touch Screen Thermostat ",
+      price: 1149,
+    },
+    {
+      image_url:
+        " https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/51/E3/96c43604-56f8-4737-8b6b-f7dce4151ced.jpg.webp  ",
+      name: " DSLR Camera Telephoto Lens Wide Angle Lens with Storage Bag   ",
+      price: 1620,
+    },
+    {
+      image_url:
+        "https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/6B/2F/30cee230-a8ca-4722-b4e0-c84b2261290f.jpg.webp",
+      name: "BlitzWolf® BW-WOO2 Bookshelf Speaker 80W Wooden Home Theater  ",
+      price: 7779,
+
+    },
+    {
+      image_url:
+        " https://imgaz.staticbg.com/thumb/gallery/oaupload/ser1/banggood/images/56/F4/c7b70aee-214e-49a2-96de-a702ec549686.jpg.webp",
+      name: " Smart WiFi GSM Home Security Alarm System Detector  ",
+      price: 3499,
+    },
+    {
+      image_url:
+        " https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/75/9C/b1ebd629-1853-4242-bf48-ad89f26f489a.jpg.webp ",
+      name: "DOOGEE V20 Global Version Dual 5G IP68 IP69K 8GB 256GB Dimensity",
+      price: 33499,
+
+    },
+    {
+      image_url:
+        "https://imgaz.staticbg.com/thumb/gallery/oaupload/banggood/images/67/63/55895816-f05c-4e81-a3f9-b9dd03142c59.jpg.webp ",
+      name: " Walkie Talkie High Power 6 Brands 136-520MHz  ",
+      price: 11399,
+
+    },
+    {
+      image_url:
+        "https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/9D/A9/7b84de76-8edc-4810-8e49-05b87cb13679.png.webp",
+      name: " Xiaomi Redmi Buds 3 Youth Edition TWS bluetooth ",
+      price: 1575,
+    },
+    {
+      image_url:
+        "https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/28/57/2d5a004c-9229-477b-bf62-b4599be02725.jpg.webp",
+      name: " OnePlus 9 5G Global Rom 8GB 128GB Snapdragon 888 6.55 inch",
+      price: 33919,
+
+    },
+    {
+      image_url:
+        "https://imgaz2.staticbg.com/thumb/large/oaupload/banggood/images/3E/8E/8d3c66b4-83f5-42dc-863b-5a65830275c5.jpg.webp",
+      name: "Rogbid Air 1.75 inch HD Screen Dual Mode Dual Chip 4G Watch",
+      price: 1620,
+    },
+    {
+      image_url:
+        "https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/F5/35/ae21292b-7668-4257-8839-e13ac6bac439.jpg.webp",
+      name: "PLEXTONE GS3 7.1 Channel Sound Card Adapter ",
+      price: 1620,
+    },
+    {
+      image_url:
+        "https://imgaz1.staticbg.com/thumb/gallery/oaupload/banggood/images/A3/92/8fca90e0-8487-4863-8b0a-5587a047320a.jpg.webp",
+      name: "TECSUN GR-88P Digital Radio Receiver Emergency Light Radio",
+      price: 7599,
+    },
+    {
+      image_url:
+        "https://imgaz2.staticbg.com/thumb/gallery/oaupload/banggood/images/69/19/0fa72ab4-5bcf-4ab2-8ccb-d626e8df9ba3.jpg.webp",
+      name: "BlitzWolf® BW-FLB5 TWS bluetooth V5.0 Earphone Gaming",
+      price: 2909,
+    },
+    {
+      image_url:
+        "https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/A3/87/6887a4c5-6f79-43fa-950c-5dc8f4d3f5cd.jpg.webp",
+      name: "Lenovo XT80 TWS bluetooth 5.1 Headphones 50ms Game/Music ",
+      price: 1779,
+    },
+    {
+      image_url:
+        "https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/0D/4F/d6af157a-b39a-479b-9bb8-441085777170.jpg.webp",
+      name: "BlitzWolf® BW-AH2 1.57 inch HD Screen 24h Heart Rate Blood Pressure ",
+      price: 1749,
+    },
+    {
+      image_url:
+        "https://imgaz3.staticbg.com/thumb/gallery/oaupload/banggood/images/0F/D6/b55b21f3-d4e3-49a9-a535-e7c9b5334715.jpg.webp",
+      name: "BlitzWolf AirAux AA-SAR1 60W bluetooth Soundbar TV Bar",
+      price: 6079,
+    },
+  ];
+
+  var cartData = JSON.parse(localStorage.getItem("cart")) || [];
+  mensData.map(function (elem) {
+    var box = document.createElement("div");
+
+    var img = document.createElement("img");
+    img.src = elem.image_url;
+
+    var name = document.createElement("p");
+    name.textContent = elem.name;
+
+    var price = document.createElement("p");
+    price.innerText = elem.price;
+
+    var btn = document.createElement("button");
+    btn.innerText = "Add to Cart";
+    btn.addEventListener("click", function () {
+      addToCart(elem);
+    });
+
+    box.append(img, name, price, btn);
+
+    document.querySelector("#container").append(box);
+  });
+
+  function addToCart(elem) {
+    cartData.push(elem);
+    localStorage.setItem("cart", JSON.stringify(cartData));
+    alert("item added to cart");
+  }
